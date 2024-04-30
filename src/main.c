6a717b89249d2e57b146821fff85831c61b749fb
Paul Bühne <64769435+Cactus-man@users.noreply.github.com>
2024-04-30 19:43:11 +0200

    include motivational message

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
 int main(void) {
-    printf("This game isn't actually about a snail.\n");
+    printf("This game isn't actually about a snail. But it will be!\n");
     return 0;
 }
