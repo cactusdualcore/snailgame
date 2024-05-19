8c08f9bc27069d2e023007ec0c0fdb4e79ff0936
Paul Bühne <64769435+Cactus-man@users.noreply.github.com>
2024-05-19 23:09:30 +0200

    Prepare game loop
    Added functions four lifecycle functions for the upcoming game
    implementation. These are 'initialize' for pre-loop setup,
    'update' and 'draw' to what their names suggest repeated in the game
    loop and finally 'cleanup' which can tidy up used resources before
    exiting.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,51 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <raylib.h>
+
+struct World {
+    
+};
+
+struct World world = {
+    
+};
+
+static const uint8_t DEFAULT_WINDOW_TITLE[] = "Snailgame";
+static const int PREFERRED_FRAME_RATE = 60;
+
+void initialize(void) {
+    const int screen_width = 800;
+    const int screen_height = 450;
+
+    InitWindow(screen_width, screen_height, "DEFAULT_WINDOW_TITLE");
+    SetTargetFPS(60);
+}
+
+void update(void) {
+
+}
+
+void draw(void) {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+    DrawText("This game isn't actually about a snail. But it will be!\n", 140, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+}
+
+void cleanup(void) {
+    CloseWindow();
+}
 
 int main(void) {
-    printf("This game isn't actually about a snail. But it will be!\n");
+    initialize();
+
+    while (!WindowShouldClose())
+    {
+        update();
+        draw();
+    }
+
     return 0;
 }
